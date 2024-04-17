75a8fb2f7cc487050b3020825ca5a435dd9e7349
Raffael Azevedo <raffael.azevedo@gmail.com>
2024-04-16 23:37:28 -0300

    added more test cases

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,5 +1,6 @@
 const { faker, fakerTH } = require("@faker-js/faker");
 const { generate } = require("gerador-validador-cpf");
+const staticData = require("../fixtures/staticData.json");
 
 const generateUser = () => {
   let birthday = faker.date.between({ from: "1950-01-01", to: "2005-12-31" });
@@ -22,12 +23,9 @@ let newUser = generateUser();
 
 describe("Feature: Registering", () => {
   beforeEach(() => {
-    //newUser = generateUser();
+    newUser = generateUser();
     cy.visit("/cadastro");
   });
-
-  /* happy path */
-
   it("should correctly open the register website", () => {
     expect(cy.get('[data-cy="button-btn-enroll"]')).to.exist;
   });
@@ -54,22 +52,114 @@ describe("Feature: Registering", () => {
       newUser.lastName,
       newUser.birthDate,
       newUser.cpf,
-      newUser.email,
+      staticData.email,
       newUser.password,
       "Advanced"
     );
     expect(cy.get("span.input-error").contains("Este email já está em uso.")).to
       .exist;
   });
+
+  it("should validate the field: CPF already registered", () => {
+    cy.get('[data-cy="button-btn-enroll"]').click();
+    cy.personalData(
+      newUser.firstName,
+      newUser.lastName,
+      newUser.birthDate,
+      staticData.cpf,
+      newUser.email,
+      newUser.password,
+      "Advanced"
+    );
+    expect(cy.get("span.input-error").contains("Este CPF já está em uso.")).to
+      .exist;
+  });
+  it("should validate the field: invalid email format", () => {
+    cy.get('[data-cy="button-btn-enroll"]').click();
+    cy.personalData(
+      newUser.firstName,
+      newUser.lastName,
+      newUser.birthDate,
+      newUser.cpf,
+      "john@doe@@mailcom",
+      newUser.password,
+      "Advanced"
+    );
+    expect(cy.get("span.input-error").contains("Email inválido.")).to.exist;
+  });
+  it.only("should validate the field: mismatching emails", () => {
+    cy.get('[data-cy="button-btn-enroll"]').click();
+    cy.get('[data-cy="input-signup-personal-data-firstName"]').type(
+      newUser.firstName
+    );
+    cy.get('[data-cy="input-signup-personal-data-lastName"]').type(
+      newUser.lastName
+    );
+    cy.get('[data-cy="input-signup-personal-data-birthDate"]').type(
+      newUser.birthDate
+    );
+    cy.get('[data-cy="input-signup-personal-data-cpf"]').type(newUser.cpf);
+    cy.get('[data-cy="input-signup-personal-data-email"]').type(newUser.email);
+    cy.get('[data-cy="input-signup-personal-data-email-confirm"]').type(
+      staticData.email
+    );
+    cy.get('[data-cy="input-signup-personal-data-password"]').type(
+      newUser.password
+    );
+    cy.get('[data-cy="input-signup-personal-data-password-confirm"]').type(
+      newUser.password
+    );
+    cy.get('[aria-controls="dropdown-button-1"]').scrollIntoView().click();
+    cy.get("span").contains("Beginner").click();
+    cy.get('[data-cy="input-signup-personal-data-lgpd"]').click();
+    cy.get('[data-cy="button-signup_submit_button_1"]').click();
+    cy.get('[data-cy="input-signup-personal-data-email-confirm"]').then(
+      ($input) => {
+        expect($input[0].validationMessage).to.eq("Os e-mails não são iguais.");
+      }
+    );
+  });
+  it("should validate the field: invalid CPF", () => {
+    cy.get('[data-cy="button-btn-enroll"]').click();
+    cy.personalData(
+      newUser.firstName,
+      newUser.lastName,
+      newUser.birthDate,
+      "1232",
+      newUser.email,
+      newUser.password,
+      "Advanced"
+    );
+    expect(cy.get("span.input-error").contains("CPF inválido.")).to.exist;
+  });
+
+  it("should validate the field: invalid birthdate", () => {
+    cy.get('[data-cy="button-btn-enroll"]').click();
+    cy.personalData(
+      newUser.firstName,
+      newUser.lastName,
+      "32133012",
+      newUser.cpf,
+      newUser.email,
+      newUser.password,
+      "Intermediate"
+    );
+    expect(cy.get("span.input-error").contains("Data de nascimento inválida."))
+      .to.exist;
+  });
 });
 
 describe("Feature: Login", () => {
   xit("should login with the registered email", () => {
     cy.visit("/sign-in");
-    cy.get("#user_login").type(newUser.email);
-    cy.get("#user_pass").type(newUser.password);
+    cy.wait(200);
+    cy.get("#user_login").type(staticData.email);
+    cy.get("#user_pass").type(staticData.password);
     cy.get("#wp-submit").click();
-    expect(cy.get("span").contains(`${newUser.firstName} ${newUser.lastName}`))
-      .to.exist;
+    cy.wait(10000);
+    cy.get(".hidden > .flex > .text-sm").should(
+      "contain.text",
+      `${staticData.firstName} ${staticData.lastName}}`
+    );
   });
 });
